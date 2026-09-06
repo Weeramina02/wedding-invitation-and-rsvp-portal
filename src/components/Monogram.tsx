446aba2d2394062f589a0a05e41656b21a635c77
Weeramina02 <112016015+Weeramina02@users.noreply.github.com>
2026-09-06 15:48:32 +0530

    Update Monogram.tsx

--- src/components/Monogram.tsx
+++ src/components/Monogram.tsx
@@ -46,7 +46,7 @@ export default function Monogram({ className = '', size = 160 }: MonogramProps) 
         textAnchor="middle"
         className="tracking-widest select-none"
       >
-        {config.monagramInitials || 'A&K'}
+        {config.monagramInitials || 'S&T'}
       </text>
 
       <path
